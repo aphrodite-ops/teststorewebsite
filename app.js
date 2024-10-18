d4f9268c8a30a4e96e289edf32526898337edb4f
aphrodite-ops <keagan.hurter@gmail.com>
2024-10-18 11:10:50 +0200

    Update app.js
    logs mouse position

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 
+var super_list=[]; //this will store the mouse position
+let mouse_pos; //this will temporarily store the moust position
+localStorage.setItem("time",0)
+
+window.addEventListener("mousemove",function get_mouse_pos(ev) { //get mouse position
+    mouse_pos=[ev.x,ev.y]; //mouse position
+    super_list=mouse_pos;
+});
+
+function loop() { //main loop
+    let super_list_serialized=JSON.stringify(super_list);
+    localStorage.setItem("superList",super_list_serialized);
+    console.log(localStorage.getItem("superList"))
+    
+};
+setInterval(loop,1); //run loop
