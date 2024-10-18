cf9a95f0f8952f108a1dc0a7675399027632c473
aphrodite-ops <keagan.hurter@gmail.com>
2024-10-18 11:40:11 +0200

    Update app.js
    detects when mouse is down

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 var super_list=[]; //this will store the mouse position
+var is_pressed=false //this will store wether or not the mouse is pressed
 let mouse_pos; //this will temporarily store the moust position
 localStorage.setItem("time",0)
 
@@ -8,10 +9,24 @@ window.addEventListener("mousemove",function get_mouse_pos(ev) { //get mouse pos
     super_list=mouse_pos;
 });
 
+//mouse down->is_pressed=true
+//mouse up->is_pressed=false
+window.addEventListener("mousedown", function get_mouse_click(ev) { //set is_pressed to true when the mouse is down
+    is_pressed=true;
+});
+window.addEventListener("mouseup", function get_mouse_up(ev) { //set is_pressed to false when the mouse is up
+    is_pressed=false;
+});
+
+
 function loop() { //main loop
     let super_list_serialized=JSON.stringify(super_list);
+    let is_pressed_serialized=JSON.stringify(is_pressed);
+    
+    localStorage.setItem("isPressed",is_pressed_serialized);
     localStorage.setItem("superList",super_list_serialized);
-    console.log(localStorage.getItem("superList"))
+    
+    console.log(localStorage.getItem("isPressed"));
     
 };
 setInterval(loop,1); //run loop
