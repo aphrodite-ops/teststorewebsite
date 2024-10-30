4af7b3368f8dc8ec73cc987baa3a77465dc2fb16
aphrodite-ops <keagan.hurter@gmail.com>
2024-10-30 15:23:06 +0200

    Update pen.js

--- pen.js
+++ pen.js
@@ -1,3 +1,5 @@
+/*line tool*/
+
 function draw_line(pos1,pos2,ctx) {
     /*pos1 is the first pos. it starts the line at pos1, and ends it at pos2*/
     ctx.beginPath();
@@ -6,7 +8,7 @@ function draw_line(pos1,pos2,ctx) {
     ctx.stroke();
 }
 
-function draw_with_pen(ctx) {
+function pen(art,ctx) {
     /*second_newest_mouse_pos was an earlier positon of the mouse, while
     newest_mouse_pos is the current position of the mouse. the way it draws 
     is by drawing small lines connecting second_newest_mouse_pos to
@@ -14,9 +16,9 @@ function draw_with_pen(ctx) {
     let second_newest_mouse_pos=art[art.length-2]; //start point of the line
     let newest_mouse_pos=art[art.length-1]; //end point of the line
 
-    //draw line
+    //draw a line
     draw_line(second_newest_mouse_pos,newest_mouse_pos,ctx);
-
+    return ['pen', [second_newest_mouse_pos,newest_mouse_pos],ctx.lineWidth,ctx.strokeStyle];
 }
 
-export {draw_with_pen };
+export { draw_line, pen };
