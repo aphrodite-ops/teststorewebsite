fca8f24ca77104018d9063deeb4922f0ca14c8cb
aphrodite-ops <keagan.hurter@gmail.com>
2024-10-30 18:52:03 +0200

    Add files via upload

---
+++ pen.js
@@ -0,0 +1,24 @@
+/*line tool*/
+
+function draw_line(pos1,pos2,ctx) {
+    /*pos1 is the first pos. it starts the line at pos1, and ends it at pos2*/
+    ctx.beginPath();
+    ctx.moveTo(pos1[0],pos1[1]);
+    ctx.lineTo(pos2[0],pos2[1]);
+    ctx.stroke();
+}
+
+function pen(art,ctx) {
+    /*second_newest_mouse_pos was an earlier positon of the mouse, while
+    newest_mouse_pos is the current position of the mouse. the way it draws 
+    is by drawing small lines connecting second_newest_mouse_pos to
+    newest_mouse_pos*/
+    let second_newest_mouse_pos=art[art.length-2]; //start point of the line
+    let newest_mouse_pos=art[art.length-1]; //end point of the line
+
+    //draw a line
+    draw_line(second_newest_mouse_pos,newest_mouse_pos,ctx);
+    return ['pen', [second_newest_mouse_pos,newest_mouse_pos],ctx.lineWidth,ctx.strokeStyle];
+}
+
+export { draw_line, pen };
