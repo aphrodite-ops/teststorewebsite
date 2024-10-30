cb870b0d8c6e0acda8e4526e0d3d64c690b64235
aphrodite-ops <keagan.hurter@gmail.com>
2024-10-30 15:24:20 +0200

    Create rectangle.js

---
+++ rectangle.js
@@ -0,0 +1,12 @@
+/*rectangle tool*/
+function rectangle(mouse_poss,ctx) {
+    let start_pos=mouse_poss[0];
+    let cur_pos=mouse_poss[1];
+
+    ctx.beginPath()
+    ctx.rect(start_pos[0],start_pos[1],cur_pos[0]-start_pos[0],cur_pos[1]-start_pos[1]);
+    ctx.stroke();
+    return ['rect',mouse_poss,ctx.lineWidth,ctx.strokeStyle];
+}
+
+export default rectangle;
