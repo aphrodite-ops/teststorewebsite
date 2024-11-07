42d32751fc558e6a2af3a7fe3ea0b29bfc0545dc
aphrodite-ops <keagan.hurter@gmail.com>
2024-11-07 17:38:36 +0200

    Update getElementVals.js

--- getElementVals.js
+++ getElementVals.js
@@ -1,16 +1,37 @@
 /*This file contains all the function to get values from the
     html elements such as sliders, textboxes, colorboxes, etc*/
 
-//get the color in the color box
-function get_pen_col() { //get the color in the color box
-    const col=document.getElementById("pen_color").value;
-    return col;
-}
+const strokeStyle=document.getElementById("pen_color");
+const tools=document.getElementById("tools");
+const lineWidth=document.getElementById("size");
+const fontSize=document.getElementById("fontSize")
+const font=document.getElementById("fonts")
 
-//get the current selected tool
-function get_tool() {
-    const tool=document.getElementById("tools").value;
-    return tool;
-}
+let stroke_col=strokeStyle.value;
+let tool=tools.value;
+let line_width=lineWidth.value;
+let font_size=fontSize.value;
+let cur_font=font.value;
 
-export { get_pen_col, get_tool };
+lineWidth.addEventListener("change", (ev) => {
+    line_width = ev.target.value
+})
+
+tools.addEventListener("change", (ev) => {
+    tool = ev.target.value
+})
+
+strokeStyle.addEventListener("change", (ev) => {
+    stroke_col=ev.target.value;
+})
+
+
+fontSize.addEventListener("change", (ev) => {
+    font_size=ev.target.value;
+})
+
+font.addEventListener("change", (ev) => {
+    cur_font=ev.target.value;
+})
+
+export { line_width, tool, stroke_col, font_size, cur_font };
